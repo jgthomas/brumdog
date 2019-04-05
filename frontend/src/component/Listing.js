25b23da323ad1bce60ef5ddeab3509d911671ae5
James Thomas <codeactor@protonmail.com>
2019-04-05 09:36:59 +0100

    remove the back button

--- frontend/src/component/Listing.js
+++ frontend/src/component/Listing.js
@@ -7,15 +7,11 @@ import './App.css';
 class Listing extends Component {
 
     render() {
-        const setPubId = this.props.setId;
         return (
           <div className="listing-box">
              <h1 className="content-title listing-title">{this.props.name}</h1>
              <img src={this.props.imageName} alt="Pub Front"/>
              <p className="rating dog-paw">{"D".repeat(this.props.rating)}</p>
-             <div className="navigation-button">
-                 <button onClick={() => setPubId(this.props.pubId)}>More</button>
-             </div>
           </div>
         );
     }
