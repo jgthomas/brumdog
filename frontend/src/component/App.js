5cbef83942dc758e012552c903c3af70814373e7
James Thomas <codeactor@protonmail.com>
2019-04-07 15:48:02 +0100

    basic support for routing to other pages

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -1,8 +1,10 @@
 
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Content from './Content';
+import RatingsGuide from './RatingsGuide';
 
 import './App.css';
 
@@ -10,10 +12,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header pageTitle="BrumDog" />
-        <Content />
-      </div>
+        <Router>
+          <div>
+            <Header pageTitle="BrumDog" />
+            <Route exact path="/" render={props => (
+                <React.Fragment>
+                <Content />
+                </React.Fragment>
+            )} />
+            <Route path="/ratings_guide" component={RatingsGuide} />
+          </div>
+        </Router>
     );
   }
 }
---
+++ frontend/src/component/RatingsGuide.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+
+const RatingsGuide = (props) => (
+    <p>Hello</p>
+);
+
+
+export default RatingsGuide;
