41c52af6a6d940b58ac6fd3887a5896759fb4210
James Thomas <codeactor@protonmail.com>
2019-04-07 11:13:34 +0100

    bring rating styling into js file

--- frontend/src/component/Rating.js
+++ frontend/src/component/Rating.js
@@ -2,20 +2,19 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import './Rating.css';
 
 class Rating extends Component {
     render() {
         return (
-            <div className="content-box rating-box">
-                <p className="rating-levels">Our paw scale of dog-friendliness</p>
-                <div className="rating-text">
-                    <div className="rating-icons">
-                        <p className="dog-paw">D</p>
-                        <p className="dog-paw">DD</p>
-                        <p className="dog-paw">DDD</p>
+            <div style={ratingBoxStyle}>
+                <p style={tagLineStyle}>{tagLine}</p>
+                <div style={ratingStyle}>
+                    <div style={ratingIconStyle}>
+                        <p>D</p>
+                        <p>DD</p>
+                        <p>DDD</p>
                     </div>
-                    <div className="rating-description">
+                    <div style={ratingWordStyle}>
                         <p>tolerated</p>
                         <p>welcomed</p>
                         <p>loved</p>
@@ -25,5 +24,39 @@ class Rating extends Component {
         );
     }
 }
+
+const ratingBoxStyle = {
+    width: '90%',
+    margin: 'auto',
+    marginTop: '3em',
+    textAlign: 'center',
+    color: '#E45641',
+    fontWeight: 'bold'
+}
+
+const tagLineStyle = {
+    fontWeight: 'normal'
+}
+
+const ratingStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: '1em'
+}
+
+const ratingIconStyle = {
+    textAlign: 'right',
+    marginRight: '1%',
+    fontSize: '120%',
+    fontFamily: 'Dingbats'
+}
+
+const ratingWordStyle = {
+    textAlign: 'left',
+    fontSize: '110%',
+    fontWeight: 'normal'
+}
+
+const tagLine = "Our paw scale of dog friendliness";
 
 export default Rating;
