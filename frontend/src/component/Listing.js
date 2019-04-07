65d081698298bf81a8b976ae736bb67c10d03804
James Thomas <codeactor@protonmail.com>
2019-04-07 10:34:46 +0100

    move all styling for listing into Listing.js

--- frontend/src/component/Listing.js
+++ frontend/src/component/Listing.js
@@ -3,21 +3,61 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './App.css';
-import './Listing.css';
 
 class Listing extends Component {
 
     render() {
         return (
-          <div className="listing-box">
-             <h1 className="content-title listing-title">{this.props.name}</h1>
-             <img src={this.props.imageName} alt="Pub Front"/>
-             <p className="rating dog-paw">{"D".repeat(this.props.rating)}</p>
-             <p className="description">{this.props.description}</p>
+          <div style={listingBoxStyle}>
+             <h1 style={titleStyle}>{this.props.name}</h1>
+             <img
+                 src={this.props.imageName}
+                 style={imageStyle}
+                 alt={imageAlt}
+             />
+             <p style={ratingStyle}>{dogPaw.repeat(this.props.rating)}</p>
+             <p style={descriptionStyle}>{this.props.description}</p>
           </div>
         );
     }
 }
+
+const listingBoxStyle = {
+    backgroundColor: 'white',
+    borderRadius: '5px',
+    border: '1px solid slategrey'
+}
+
+const titleStyle = {
+    textAlign: 'center',
+    color: 'dodgerblue'
+}
+
+const imageStyle = {
+    display: 'block',
+    width: '80%',
+    margin: 'auto',
+    height: 'auto'
+}
+
+const ratingStyle = {
+    fontWeight: 'bold',
+    fontSize: '120%',
+    fontFamily: 'Dingbats',
+    textAlign: 'center',
+    color: '#E45641'
+}
+
+const descriptionStyle = {
+    width: '90%',
+    margin: 'auto',
+    color: 'dimgrey'
+}
+
+const dogPaw = "D";
+
+const imageAlt = "Photo of pub";
+
 
 Listing.propTypes = {
     name: PropTypes.string,
